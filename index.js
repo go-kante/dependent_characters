271a730aa366582d6f3d1bbe58a1273ac3fd710d
go-kante <leisk0422@gmail.com>
2024-12-06 19:20:42 +0900

    ss

---
+++ index.js
@@ -0,0 +1,17 @@
+JudgeText()=
+    let x = document.getElementById('form').onsubmit 
+    function (event) {
+
+    }
+        event.preventDefault();
+        const list1 = ["a,i"];
+            If (list1 === content) {
+                let inputForm = document.getElementById('form').content.value;
+		        // 入力内容を画面に出力
+                document.getElementById('form').textContent.style.textColor = 'red'
+            } else if (list1 != content) {
+                console.log（"環境依存文字は有りません。"）;
+            }
+        
+
+
