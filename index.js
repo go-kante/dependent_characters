4b935d2726a7ddde3d4290ab1cac21ecdf855d7c
go-kante <leisk0422@gmail.com>
2024-12-09 00:18:32 +0900

    途中経過

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-JudgeText()=
-    let x = document.getElementById('form').onsubmit 
-    function (event) {
+//テキストボクスの内容を定義
+const x = document.getElementById('check').content.value;/*checkの情報を取得*/
+x.addEventListener('click', changeTextColor);/*イベント クリック時テキスト色を変える関数*/
+//配列の中身
+var character = [a,i,u,e,o];
+console.log(character.length);
+console.log(x.value);
 
-    }
-        event.preventDefault();
-        const list1 = ["a,i"];
-            If (list1 === content) {
-                let inputForm = document.getElementById('form').content.value;
-		        // 入力内容を画面に出力
-                document.getElementById('form').textContent.style.textColor = 'red'
-            } else if (list1 != content) {
-                console.log（"環境依存文字は有りません。"）;
+function changeTextColor() {
+    if (character == "check".click) {
+        document.getElementById("check").style.TextColor = "blue";
+
+
+    // const x = document.getElementById('check').content.value;}
+
+    // function (event) {
+
+    // }
+    //     event.preventDefault();
+    //     const list1 = ["a,i"];
+    //         If (list1 === content) {
+    //             let inputForm = document.getElementById('form').content.value;
+	// 	        // 入力内容を画面に出力
+    //             document.getElementById('form').textContent.style.textColor = 'red'
+    //         } else if (list1 != content) {
+    //             console.log（"環境依存文字は有りません。"）;
             }
+}
         
 
 
