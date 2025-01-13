636f9fccffe22e667a5253f3ef404c17b29f2758
go-kante <leisk0422@gmail.com>
2025-01-13 23:44:53 +0900

    バックエンド　データベース初期設計

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+app.get('/', (req, res) => {
+  connection.query(
+    'SELECT * FROM user',
+    (error, results) => {
+      console.log(results);
+      res.render('hello.ejs');
+    }
+  );
+});
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'sk0422OK  ',
+  password: 'sk0422OK',
   database: 'detect_d_chara'
 });
 
-const express = require('express');
+connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('success');
+  });
 
-const app = express();
+  
 
-app.get('/', (req, res) => {
-  res.render('hello.ejs');
-});
-
-app.listen(3000);
